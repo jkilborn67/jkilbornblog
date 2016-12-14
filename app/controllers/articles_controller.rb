be724bbd5d2bbc22779a9721adf873947bd296cb
Jim Kilborn <jim@kilborns.com>
2016-12-14 13:19:07 +0000

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,59 @@
+class ArticlesController < ApplicationController
+  
+  # Get the article before the listed methods start
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  
+  def new
+    @article = Article.new
+  end
+  
+  def index
+    @articles = Article.all
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    if @article.update(article_params)
+      flash[:notice] = "Article was sucessfully updated."
+      redirect_to article_path(@article)
+    else
+      render "edit"
+    end
+  end
+  
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      #render plain: params[:article].inspect
+      flash[:notice] = "Article saved sucessfully."
+      redirect_to article_path(@article)
+    else
+      render "new"
+    end
+  end
+   
+  def show
+    
+  end
+  
+  def destroy
+    
+    @article.destroy
+    flash[:notice] = 'Articles was sucessfully deleted'
+    redirect_to articles_path
+  end
+  
+   
+  private
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+    
+    def set_article
+      @article = Article.find(params[:id])
+    end
+end
